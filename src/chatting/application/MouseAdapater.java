b7b24252e9f7c996960bdb1aa97da172992c2033
Kushagratyagi28 <114990334+Kushagratyagi28@users.noreply.github.com>
2023-07-28 18:31:15 +0530

    Add files via upload

---
+++ src/chatting/application/MouseAdapater.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package chatting.application;
+
+import java.awt.event.MouseListener;
+
+/**
+ *
+ * @author DELL
+ */
+public class MouseAdapater implements MouseListener {
+
+    public MouseAdapater() {
+    }
+    
+}
